ea235ef858adef38613f606bb08ff65e72545143
Ricardo <r3db@users.noreply.github.com>
2025-01-24 20:16:37 +0000

    Update MotionSensor.js

--- MotionSensor.js
+++ MotionSensor.js
@@ -1,5 +1,5 @@
-let maximumHumidity    = 75;
-let maximumTemperature = 24;
+let maximumHumidity    = 75; // Todo: Read from config async!
+let maximumTemperature = 24; // Todo: Read from config async!
 
 let humidity;
 let temperature;
@@ -7,23 +7,12 @@ let temperature;
 let hasHumidityReading    = false;
 let hasTemperatureReading = false;
 
+let startHour; // Todo: Read from config async!
+let endHour;   // Todo: Read from config async!
+
 let autoOffDelay = 5 * 60;
 
-Shelly.addStatusHandler(function(e) {
-  if (e.component === "input:100") {
-    handleMotion(e.delta.state);
-  } else if (e.component === "humidity:100") {
-    handleHumidity(e.delta.rh);
-  } else if (e.component === "temperature:100") {
-    handleTemperature(e.delta.tC);
-  } else if (e.component === "number:201") {
-    handleMaximumTemperature(e.delta.tC);
-  } else if (e.component === "number:200") {
-    handleMaximumHumidity(e.delta.rh);
-  }
-});
-
-function handleMotion(value) {
+function handleMotionChange(value) {
   Shelly.call("Number.Set", {'id': 202, 'value': value === true ? 1 : 0});
     
   if (value === true) {
@@ -31,7 +20,18 @@ function handleMotion(value) {
   }
 }
 
-function handleTemperature(value) {
+// Todo: Incorporate Time!
+function handleTemperature() {
+  if (hasTemperatureReading === false) {
+  	Shelly.call("Temperature.GetStatus", {'id': 100}, function(result, err_code, err_message) {
+  	  handleTemperatureChange(result.tC);
+    });
+  } else {
+    handleTemperatureChange(temperature);
+  }
+}
+
+function handleTemperatureChange(value) {
   temperature = value;
   hasTemperatureReading = true;
   
@@ -40,7 +40,18 @@ function handleTemperature(value) {
   }
 }
 
-function handleHumidity(value) {
+// Todo: Incorporate Time!
+function handleHumidity() {
+  if (hasHumidityReading === false) {
+  	Shelly.call("Humidity.GetStatus", {'id': 100}, function(result, err_code, err_message) {
+  	  handleHumidityChange(result.rh);
+    });
+  } else {
+    handleHumidityChange(humidity);
+  }
+}
+
+function handleHumidityChange(value) {
   humidity = value;
   hasHumidityReading = true;
     
@@ -49,30 +60,43 @@ function handleHumidity(value) {
   }
 }
 
-function handleMaximumTemperature(value) {
+function handleMaximumTemperatureChange(value) {
   maximumTemperature = value;
-    
-  if (hasTemperatureReading === false) {
-    Shelly.call("Temperature.GetStatus", {'id': 100}, function(result) {
-      handleTemperature(result.tC);
-    });
-  } else {
-    handleTemperature(temperature);
-  }
+  handleTemperature();
 }
 
-function handleMaximumHumidity(value) {
+function handleMaximumHumidityChange(value) {
   maximumHumidity = value;
-    
-  if (hasHumidityReading === false) {
-    Shelly.call("Humidity.GetStatus", {'id': 100}, function(result) {
-      handleHumidity(result.rh);
-    });
-  } else {
-    handleHumidity(humidity);
-  }
+  handleHumidity();
 }
 
 function switchOn() {
   Shelly.call("Switch.Set", {'id': 0, 'on': true, 'toggle_after': autoOffDelay});
 }
+
+Shelly.addStatusHandler(function(e) {
+  if (e.component === "input:100") {
+    handleMotionChange(e.delta.state);
+  } else if (e.component === "temperature:100") {
+    handleTemperatureChange(e.delta.tC);
+  } else if (e.component === "humidity:100") {
+    handleHumidityChange(e.delta.rh);
+  } else if (e.component === "number:201") {
+    handleMaximumTemperatureChange(e.delta.value);
+  } else if (e.component === "number:200") {
+    handleMaximumHumidityChange(e.delta.value);
+  }
+});
+
+Timer.set(5000, true, function() {
+	handleTemperature();
+	handleHumidity();
+});
+
+// -------------------------------
+
+Shelly.call("Sys.GetStatus", {}, function(result, err_code, err_message, user_data) {
+	if (err_code === 0) {
+		console.log("Result:", result.time);
+	}
+});
