1e1bd47d16ccb32217d3097259fe900a29504eae
Ricardo <r3db@users.noreply.github.com>
2025-01-26 21:39:16 +0000

    Update MotionSensor.js
    Provided Support for false positives

--- MotionSensor.js
+++ MotionSensor.js
@@ -1,102 +1,144 @@
-let maximumHumidity    = 75; // Todo: Read from config async!
-let maximumTemperature = 24; // Todo: Read from config async!
+// State
+let humidity    = null;
+let temperature = null;
 
-let humidity;
-let temperature;
+let previousSensorTime = null;
+let currentSensorTime  = null;
 
-let hasHumidityReading    = false;
-let hasTemperatureReading = false;
+// Configurable [via Virtual Components]
+let maximumHumidity    = null;
+let maximumTemperature = null;
 
-let startHour; // Todo: Read from config async!
-let endHour;   // Todo: Read from config async!
+// Configurable [via Variables]
+let silentHourStart = 0;
+let silentHourEnd   = 7;
+let currentHour     = null;
+let currentMinute   = null;
+let autoOffDelay    = 5 * 60;
 
-let autoOffDelay = 5 * 60;
+// Constants
+const SensorTrue  = 1;
+const SensorFalse = 0;
+
+function init() {
+  updateCurrentHour();
+
+  Shelly.call("Number.GetStatus", {'id': 200}, function(result) {
+    updateSensorBasedOnMaximumHumidityChange(result.value);
+  });
+
+  Shelly.call("Number.GetStatus", {'id': 201}, function(result) {
+    updateSensorBasedOnMaximumTemperatureChange(result.value);
+  });
+
+  Timer.set(10 * 1000, true, function() {
+    updateCurrentHour();
+    updateSensorBasedOnCurrentHumidity();
+    updateSensorBasedOnCurrentTemperature();
+  });
+}
 
 function handleMotionChange(value) {
-  Shelly.call("Number.Set", {'id': 202, 'value': value === true ? 1 : 0});
-    
-  if (value === true) {
+  if (value === false) {
+  	Shelly.call("Number.Set", {'id': 202, 'value': SensorFalse});
+  	return;
+  }
+
+  currentSensorTime = Date.now();
+
+  if (previousSensorTime === null) {
+  	previousSensorTime = Date.now();
+  	return;
+  }
+
+  let diff = (currentSensorTime - previousSensorTime) / 1000;
+    	
+  if (diff < 30) {
+    Shelly.call("Number.Set", {'id': 202, 'value': SensorTrue});
     switchOn();
   }
+      
+  previousSensorTime = currentSensorTime;
 }
 
-// Todo: Incorporate Time!
-function handleTemperature() {
-  if (hasTemperatureReading === false) {
-  	Shelly.call("Temperature.GetStatus", {'id': 100}, function(result, err_code, err_message) {
-  	  handleTemperatureChange(result.tC);
+function updateSensorBasedOnCurrentHumidity() {
+  if (humidity === null) {
+    Shelly.call("Humidity.GetStatus", {'id': 100}, function(result) {
+      updateSensorBasedOnHumidityChange(result.rh);
     });
   } else {
-    handleTemperatureChange(temperature);
+    updateSensorBasedOnHumidityChange(humidity);
   }
 }
 
-function handleTemperatureChange(value) {
-  temperature = value;
-  hasTemperatureReading = true;
-  
-  if (value >= maximumTemperature) {
-    switchOn();
-  }
-}
-
-// Todo: Incorporate Time!
-function handleHumidity() {
-  if (hasHumidityReading === false) {
-  	Shelly.call("Humidity.GetStatus", {'id': 100}, function(result, err_code, err_message) {
-  	  handleHumidityChange(result.rh);
-    });
-  } else {
-    handleHumidityChange(humidity);
-  }
-}
-
-function handleHumidityChange(value) {
+function updateSensorBasedOnHumidityChange(value) {
   humidity = value;
-  hasHumidityReading = true;
+
+  if (currentHour != null && currentHour >= silentHourStart && currentHour <= silentHourEnd) {
+    return;
+  }
     
   if (value >= maximumHumidity) {
     switchOn();
   }
 }
 
-function handleMaximumTemperatureChange(value) {
-  maximumTemperature = value;
-  handleTemperature();
+function updateSensorBasedOnCurrentTemperature() {
+  if (temperature === null) {
+    Shelly.call("Temperature.GetStatus", {'id': 100}, function(result) {
+      updateSensorBasedOnTemperatureChange(result.tC);
+    });
+  } else {
+    updateSensorBasedOnTemperatureChange(temperature);
+  }
 }
 
-function handleMaximumHumidityChange(value) {
+function updateSensorBasedOnTemperatureChange(value) {
+  temperature = value;
+  
+  if (currentHour != null && currentHour >= silentHourStart && currentHour <= silentHourEnd) {
+    return;
+  }
+
+  if (value >= maximumTemperature) {
+    switchOn();
+  }
+}
+
+function updateSensorBasedOnMaximumHumidityChange(value) {
   maximumHumidity = value;
-  handleHumidity();
+  updateSensorBasedOnCurrentHumidity();
+}
+
+function updateSensorBasedOnMaximumTemperatureChange(value) {
+  maximumTemperature = value;
+  updateSensorBasedOnCurrentTemperature();
 }
 
 function switchOn() {
   Shelly.call("Switch.Set", {'id': 0, 'on': true, 'toggle_after': autoOffDelay});
 }
 
+function updateCurrentHour() {
+  Shelly.call("Sys.GetStatus", {}, function(result) {
+    let time = result.time.split(":");
+    currentHour   = time[0];
+    currentMinute = time[1];
+  });
+}
+
 Shelly.addStatusHandler(function(e) {
   if (e.component === "input:100") {
     handleMotionChange(e.delta.state);
-  } else if (e.component === "temperature:100") {
-    handleTemperatureChange(e.delta.tC);
   } else if (e.component === "humidity:100") {
-    handleHumidityChange(e.delta.rh);
-  } else if (e.component === "number:201") {
-    handleMaximumTemperatureChange(e.delta.value);
+    updateSensorBasedOnHumidityChange(e.delta.rh);
+  } else if (e.component === "temperature:100") {
+    updateSensorBasedOnTemperatureChange(e.delta.tC);
   } else if (e.component === "number:200") {
-    handleMaximumHumidityChange(e.delta.value);
+    updateSensorBasedOnMaximumHumidityChange(e.delta.value);
+  } else if (e.component === "number:201") {
+    updateSensorBasedOnMaximumTemperatureChange(e.delta.value);
   }
 });
 
-Timer.set(5000, true, function() {
-	handleTemperature();
-	handleHumidity();
-});
-
-// -------------------------------
-
-Shelly.call("Sys.GetStatus", {}, function(result, err_code, err_message, user_data) {
-	if (err_code === 0) {
-		console.log("Result:", result.time);
-	}
-});
+init();
