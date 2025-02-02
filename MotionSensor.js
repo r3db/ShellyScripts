ca9c8a811e601e7fcc46eb4891cea9a07866311c
Ricardo <r3db@users.noreply.github.com>
2025-02-02 16:16:13 +0000

    Update MotionSensor.js

--- MotionSensor.js
+++ MotionSensor.js
@@ -1,13 +1,14 @@
 // State
-let humidity    = null;
-let temperature = null;
+let humidity     = null;
+let temperature  = null;
+let sensorHandle = Virtual.getHandle("number:202");
 
 let previousSensorTime = null;
 let currentSensorTime  = null;
 
 // Configurable [via Virtual Components]
-let maximumHumidity    = null;
-let maximumTemperature = null;
+let maximumHumidity    = Virtual.getHandle("number:200").getValue();
+let maximumTemperature = Virtual.getHandle("number:201").getValue();
 
 // Configurable [via Variables]
 let silentHourStart = 0;
@@ -23,24 +24,20 @@ const SensorFalse = 0;
 function init() {
   updateCurrentHour();
 
-  Shelly.call("Number.GetStatus", {'id': 200}, function(result) {
-    updateSensorBasedOnMaximumHumidityChange(result.value);
-  });
-
-  Shelly.call("Number.GetStatus", {'id': 201}, function(result) {
-    updateSensorBasedOnMaximumTemperatureChange(result.value);
-  });
-
   Timer.set(10 * 1000, true, function() {
-    updateCurrentHour();
-    updateSensorBasedOnCurrentHumidity();
-    updateSensorBasedOnCurrentTemperature();
+    try {
+      updateCurrentHour();
+      updateSensorBasedOnCurrentHumidity();
+      updateSensorBasedOnCurrentTemperature();
+    } catch (error) {
+      console.error(error);
+    }
   });
 }
 
 function handleMotionChange(value) {
   if (value === false) {
-    Shelly.call("Number.Set", {'id': 202, 'value': SensorFalse});
+    sensorHandle.setValue(SensorFalse);
     return;
   }
 
@@ -54,7 +51,7 @@ function handleMotionChange(value) {
   let diff = (currentSensorTime - previousSensorTime) / 1000;
       
   if (diff < 30) {
-    Shelly.call("Number.Set", {'id': 202, 'value': SensorTrue});
+    sensorHandle.setValue(SensorTrue);
     switchOn();
   }
       
@@ -128,16 +125,20 @@ function updateCurrentHour() {
 }
 
 Shelly.addStatusHandler(function(e) {
-  if (e.component === "input:100") {
-    handleMotionChange(e.delta.state);
-  } else if (e.component === "humidity:100") {
-    updateSensorBasedOnHumidityChange(e.delta.rh);
-  } else if (e.component === "temperature:100") {
-    updateSensorBasedOnTemperatureChange(e.delta.tC);
-  } else if (e.component === "number:200") {
-    updateSensorBasedOnMaximumHumidityChange(e.delta.value);
-  } else if (e.component === "number:201") {
-    updateSensorBasedOnMaximumTemperatureChange(e.delta.value);
+  try {
+    if (e.component === "input:100") {
+      handleMotionChange(e.delta.state);
+    } else if (e.component === "humidity:100") {
+      updateSensorBasedOnHumidityChange(e.delta.rh);
+    } else if (e.component === "temperature:100") {
+      updateSensorBasedOnTemperatureChange(e.delta.tC);
+    } else if (e.component === "number:200") {
+      updateSensorBasedOnMaximumHumidityChange(e.delta.value);
+    } else if (e.component === "number:201") {
+      updateSensorBasedOnMaximumTemperatureChange(e.delta.value);
+    }
+  } catch (error) {
+    console.error(error);
   }
 });
 
